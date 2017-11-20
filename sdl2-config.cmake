42aab3d991b63815756ac19a2a9bd72dbc2d0047
Tim Simpson <timsimpson4@gmail.com>
2017-11-20 00:19:46 -0600

    More libs

--- sdl2-config.cmake
+++ sdl2-config.cmake
@@ -20,6 +20,27 @@ set_property(TARGET sdl2::sdl2 PROPERTY IMPORTED_LOCATION "${SDL2}")
 set_target_properties(sdl2::sdl2 PROPERTIES
   INTERFACE_INCLUDE_DIRECTORIES "${_IMPORT_PREFIX}/include")
 
+
+find_library(SDL2_ttf NAMES "SDL2_ttf" PATHS "${CMAKE_CURRENT_SOURCE_DIR}/lib")
+add_library(sdl2::ttf STATIC IMPORTED)
+set_property(TARGET sdl2::ttf PROPERTY IMPORTED_LOCATION "${SDL2_ttf}")
+set_target_properties(sdl2::ttf PROPERTIES
+  INTERFACE_LINK_LIBRARIES sdl2::sdl2)
+
+
+find_library(SDL2_image NAMES "SDL2_image" PATHS "${CMAKE_CURRENT_SOURCE_DIR}/lib")
+add_library(sdl2::image STATIC IMPORTED)
+set_property(TARGET sdl2::image PROPERTY IMPORTED_LOCATION "${SDL2_image}")
+set_target_properties(sdl2::image PROPERTIES
+  INTERFACE_LINK_LIBRARIES sdl2::sdl2)
+
+
+find_library(SDL2_mixer NAMES "SDL2_mixer" PATHS "${CMAKE_CURRENT_SOURCE_DIR}/lib")
+add_library(sdl2::mixer STATIC IMPORTED)
+set_property(TARGET sdl2::mixer PROPERTY IMPORTED_LOCATION "${SDL2_mixer}")
+set_target_properties(sdl2::mixer PROPERTIES
+  INTERFACE_LINK_LIBRARIES sdl2::sdl2)
+
 # Cleanup temporary variables.
 set(_IMPORT_PREFIX)
 
