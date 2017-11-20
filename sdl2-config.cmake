4e4555d607a1dbeadcd3228f85e22b1af5757d71
Tim Simpson <timsimpson4@gmail.com>
2017-11-20 01:54:00 -0600

    Clean up include path mechanism

--- sdl2-config.cmake
+++ sdl2-config.cmake
@@ -14,6 +14,15 @@ if(_IMPORT_PREFIX STREQUAL "/")
   set(_IMPORT_PREFIX "")
 endif()
 
+find_path(_INCLUDE_DIR SDL.h
+  PATHS
+  "${_IMPORT_PREFIX}/include"
+  "/usr/include"
+  "/usr/include/SDL2"
+  "/usr/local/include"
+  "/usr/local/include/SDL2"
+)
+
 find_library(SDL2 NAMES "SDL2"
   PATHS
   "${CMAKE_CURRENT_SOURCE_DIR}/lib"
@@ -21,7 +30,7 @@ find_library(SDL2 NAMES "SDL2"
 add_library(sdl2::sdl2 STATIC IMPORTED)
 set_property(TARGET sdl2::sdl2 PROPERTY IMPORTED_LOCATION "${SDL2}")
 set_target_properties(sdl2::sdl2 PROPERTIES
-  INTERFACE_INCLUDE_DIRECTORIES "${_IMPORT_PREFIX}/include")
+  INTERFACE_INCLUDE_DIRECTORIES "${_INCLUDE_DIR}")
 
 
 find_library(SDL2_ttf NAMES "SDL2_ttf"
@@ -55,5 +64,10 @@ set_target_properties(sdl2::mixer PROPERTIES
 
 # Cleanup temporary variables.
 set(_IMPORT_PREFIX)
+set(_INCLUDE_DIR)
+set(SDL2)
+set(SDL2_ttf)
+set(SDL2_image)
+set(SDL2_mixer)
 
 set(CMAKE_IMPORT_FILE_VERSION)
